6a1295f0e4ed702e6fcccc46f5acf0e984fea270
18822835767 <1165407654@qq.com>
2020-04-23 10:03:53 +0800

    连接池跑起来了

--- src/model/database/ConnectionProxy.java
+++ src/model/database/ConnectionProxy.java
@@ -9,15 +9,14 @@ import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.ResourceBundle;
 
-public class ConnectionProxy extends DataSourceAdapter {
+public class ConnectionProxy extends DataSourceAdapter{
     private static int initConnections = 5;
-    private static int maxConnections = 10;//空闲池允许的最大的连接数量
-    private static LinkedList<Connection> freePools = new LinkedList<>();//空闲的连接池
-    private static LinkedList<Connection> activePools = new LinkedList<>();//使用中的连接池
-    private static int maxTotalConnections = 30;//允许的最大的连接总数.
-    private static int count = 0;//记录已开启的连接
+    private static int poolMaxIdleConnections = 10;//空闲池允许的最大的连接数量
+    private static int poolMaxActiveConnections = 10;
+    private static LinkedList<Connection> idleConnections = new LinkedList<>();//空闲的连接池
+    private static LinkedList<Connection> activeConnections = new LinkedList<>();//激活的连接池
 
-    boolean end = false;//判断程序是否结束。
+    private final Object monitor = new Object();//用于同步
 
     static {
         try {
@@ -29,8 +28,9 @@ public class ConnectionProxy extends DataSourceAdapter {
             Class.forName(driver);
             //初始化连接池的数量
             for (int i = 0; i < initConnections; i++) {
-                freePools.addFirst(DriverManager.getConnection(url, username, password));
+                idleConnections.addFirst(DriverManager.getConnection(url, username, password));
             }
+            System.out.println("初始化后空闲连接池:"+idleConnections.size()+"  激活连接池:"+activeConnections.size());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -38,19 +38,78 @@ public class ConnectionProxy extends DataSourceAdapter {
 
     @Override
     public Connection getConnection() throws SQLException {
-        Connection conn = freePools.removeLast();
-        //返回代理
-        return (Connection) Proxy.newProxyInstance(conn.getClass().getClassLoader(), new Class[]{Connection.class},
+        Connection conn = null;
+
+        while(conn == null){
+            synchronized (monitor){
+                //如果空闲连接池不为空，直接获取连接
+                if(!idleConnections.isEmpty()){
+                    conn = idleConnections.remove(0);
+                }else{
+                    //没有空闲连接可以使用，那么我们需要获取新的连接(也就是需要传建一个连接)
+                    if(activeConnections.size() < poolMaxActiveConnections){
+                        //如果当前激活的连接数 小于 我们允许的最大连接数，那么此时可以创建一个新的连接，否则还不能创建
+                        conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/dragonsystem","root",
+                                "333");
+                    }
+                    //否则是不能创建新连接的，需要等待，wait
+                }
+            }
+
+            if(conn == null){
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    //线程给打断则跳出循环
+                    break;
+                }
+            }
+
+        }
+
+        if(conn != null){
+            //对象不为空，说明已经拿到了该连接
+            activeConnections.add(conn);
+            System.out.println("得到连接后空闲连接池:"+idleConnections.size()+"  激活连接池:"+activeConnections.size());
+        }
+        //返回连接对象
+        Connection finalConn = conn;
+        return (Connection) Proxy.newProxyInstance(Connection.class.getClassLoader(), new Class[]{Connection.class},
                 new InvocationHandler() {
                     @Override
                     public Object invoke(Object o, Method method, Object[] args) throws Throwable {
                         if ("close".equals(method.getName())) {
-                            freePools.addFirst(conn);
+                            synchronized (monitor){
+                                activeConnections.remove(finalConn);
+                                if(idleConnections.size() < poolMaxIdleConnections){
+                                    idleConnections.addFirst(finalConn);
+                                }
+                                monitor.notifyAll();
+
+                                System.out.println("关闭连接后空闲连接池:"+idleConnections.size()+"  激活连接池:"+activeConnections.size());
+                            }
                         } else {
-                            return method.invoke(conn, args);
+                            return method.invoke(finalConn, args);
                         }
                         return null;
                     }
                 });
+
+//        Connection conn = idleConnections.removeLast();
+//        //返回代理
+//        return (Connection) Proxy.newProxyInstance(conn.getClass().getClassLoader(), new Class[]{Connection.class},
+//                new InvocationHandler() {
+//                    @Override
+//                    public Object invoke(Object o, Method method, Object[] args) throws Throwable {
+//                        if ("close".equals(method.getName())) {
+//                            idleConnections.addFirst(conn);
+//                        } else {
+//                            return method.invoke(conn, args);
+//                        }
+//                        return null;
+//                    }
+//                });
     }
+
 }
